ae351d4d11bf7fc2684ef63bc916f78199a8f1a9
nimscoding <nhiem.codes@gmail.com>
2021-05-19 19:03:02 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="chooseContainer">
+      <div className="chooseWrap">
+        <div className="contentWrap">
+          <div className="beforeTitle">no code needed </div>
+          <div className="title">Start with a template</div>
+          <div className="text">
+            Each design comes with a selection of premade layouts and content
+            blocks to help you build a professional website, fast. Customize
+            fonts, colors and the structure of your pages. Enjoy maximum control
+            over your desktop and mobile site.
+          </div>
+          <div className="sliderWrap">
+            <a className="aStyle" href="www.youtube.com">
+              <div
+                className="divStyle"
+                style={{ backgroundImage: `url(/images/JuneTheme.jpg)` }}
+              ></div>
+              <div className="desc1">
+                mono/
+                <span> Minimal &amp; Versatile </span>
+              </div>
+            </a>
+            <a className="aStyle" href="www.youtube.com">
+              <div
+                className="divStyle"
+                style={{ backgroundImage: `url(/images/MonoTheme.jpg)` }}
+              ></div>
+              <div className="desc1">
+                June/
+                <span> Bossy, Fun &amp; Stylish</span>
+              </div>
+            </a>
+            <a className="aStyle" href="www.youtube.com">
+              <div
+                className="divStyle"
+                style={{ backgroundImage: `url(/images/VelvetTheme.jpg)` }}
+              ></div>
+              <div className="desc1">
+                Velvet/
+                <span> Minimal &amp; Versatile </span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
-
-export default App;
